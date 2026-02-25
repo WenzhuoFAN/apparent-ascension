6f32d443d535e8b1f0821b5ad9c63b6236954acc
WenXFan <1363764368w@gmail.com>
2026-02-26 03:13:49 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    server: {
+      allowedHosts: true,
+    },
     preview: {
-      allowedHosts: [
-        'fg-fansite-app-d9f490124f97.herokuapp.com',
-        '.herokuapp.com',
-      ],
+      allowedHosts: true,
     },
   },
 });
